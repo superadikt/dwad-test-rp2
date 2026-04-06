faec7984e8f32fefcec899ed49a69ccb9e4721f7
diomar.villarosa <diomar.villarosa@dict.gov.ph>
2026-04-06 16:40:08 +0800

    Add TestController with Index action returning a view
    Introduced TestController in the DwadTestRp.Controllers namespace. The controller inherits from Controller and provides an Index action that returns a view. Added the required using directive for Microsoft.AspNetCore.Mvc.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DwadTestRp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
